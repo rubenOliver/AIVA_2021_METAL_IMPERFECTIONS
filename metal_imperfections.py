b7ba63f93337465bc6549554175a5dfa47db9d95
juanlu-urjc <jl.carrillo.2020@alumnos.urjc.es>
2021-03-15 18:42:09 +0100

    #2 Arquitectura programa y tests

---
+++ metal_imperfections.py
@@ -0,0 +1,18 @@
+class MetalImperfections:
+    def splitData(self, path):
+        pass
+
+    def loadTestData(self, path):
+        pass
+
+    def loadTrainData(self, path):
+        pass
+
+    def loadValidData(self, path):
+        pass
+
+    def train(self, path):
+        pass
+
+    def recognize(self, image):
+        pass
