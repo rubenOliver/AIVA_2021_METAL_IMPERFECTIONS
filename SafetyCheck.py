b6ee56cb4695f951e27bdf825717ead0d73d3506
juanlu-urjc <jl.carrillo.2020@alumnos.urjc.es>
2021-03-18 23:00:07 +0100

    #2 Modificación del mockup de la librería y de los tests

--- SafetyCheck.py
+++ SafetyCheck.py
@@ -6,10 +6,23 @@ from PIL import Image, ImageOps
 
 class MetalImperfections:
 
-    def recognize(self, image):
-        pass
+    def recognize(self, path_image):
+        '''
+        Clasifica la imagen y a continuación se llama al método
+        __getBndbox__ para obtener una lista de bounding boxes
+        :param path_image: Ruta del fichero de la imagen
+        :return: Tupla con la predicción del error y la lista de bounding boxes
+        '''
+        return 'label', []
 
-    def getBndbox(self, image, label):
+
+    def __getBndbox__(self, image, label):
+        '''
+        Calcula la lista de boundig boxes de una imagen de un determinado fallo.
+        :param image: La imagen
+        :param label: Etiqueta clasificada
+        :return: Lista de bounding boxes
+        '''
         pass
 
 if __name__ == '__main__':
--- SafetyCheck_test.py
+++ SafetyCheck_test.py
@@ -3,79 +3,118 @@ Fichero con los test unitarios del entrenamiento y del reconocedor
 """
 
 import unittest
-# import tensorflow as tf
 from SafetyCheck import MetalImperfections
-import csv
 import os
 from xml.dom import minidom
 
+
+def __get_intersection__(bb_results, bndboxs):
+    '''
+    Se calculan las intersecciones
+    :param bb_results: Los bounding-boxes del detector
+    :param bndboxs: Los bounding-boxes del ground-truth
+    :return: El número de intersecciones
+    '''
+    intersections = 0
+    for bb_result in bb_results:
+        for bndbox in bndboxs:
+            xA = max(bb_result[0], bndbox[0])
+            yA = max(bb_result[1], bndbox[1])
+            xB = min(bb_result[2], bndbox[2])
+            yB = min(bb_result[3], bndbox[3])
+
+            # compute the area of intersection rectangle
+            interArea = abs(max((xB - xA, 0)) * max((yB - yA), 0))
+            if interArea > 0:
+                intersections += 1
+    return intersections
+
+
+def __get_gt_bndbox__(path_anno):
+    '''
+    Obtiene los bounding-boxes del ground-truth
+    :param path_anno: Ruta de los documentos de anotaciones
+    :return: Lista de bounding-boxes del ground-truth
+    '''
+    bndboxs = []
+    doc = minidom.parse(path_anno)
+    objects = doc.getElementsByTagName("object")
+    for object in objects:
+        xmin = object.getElementsByTagName("xmin")[0].firstChild.data
+        ymin = object.getElementsByTagName("ymin")[0].firstChild.data
+        xmax = object.getElementsByTagName("xmax")[0].firstChild.data
+        ymax = object.getElementsByTagName("ymax")[0].firstChild.data
+        bndbox = [xmin, ymin, xmax, ymax]
+        bndboxs.append(bndbox)
+
+    return bndboxs
+
+
+def __test_classifier__(dir_test_path, label):
+    '''
+    Calcula los aciertos y los fallos de una serie de imagenes de test de un determinado tipo
+    :param dir_test_path: Ruta de la carpeta donde se encuentran las imágenes de test
+    :param label: Tipo de imperfección del metal
+    :return: Número de aciertos y de fallos
+    '''
+    files = os.listdir(dir_test_path)
+
+    metal_imperfections = MetalImperfections()
+    success = 0
+    failure = 0
+    for file in files:
+        result = metal_imperfections.recognize(file)
+        if result == label:
+            success += 1
+        else:
+            failure += 1
+    return success, failure
+
+
 class TestMetalTrain(unittest.TestCase):
-    def test_getBndbox(self):
+    def test_recognize(self):
+        '''
+        Comprueba la etiqueta y que al menos un bounding-box intersecciones con el del ground-truth de una de las
+        imágenes
+        :return:
+        '''
+        # Cambiar el path y el label por otras imágenes (patches, scratches, ...)
         path_image = './NEU-DET/IMAGES/inclusion_1.jpg'
         path_anno = './NEU-DET/ANNOTATIONS/inclusion_1.xml'
         label = 'inclusion'
-
-        bndboxs = []
-        doc = minidom.parse(path_anno)
-        objects = doc.getElementsByTagName("object")
-        for object in objects:
-            xmin = object.getElementsByTagName("xmin")[0].firstChild.data
-            ymin = object.getElementsByTagName("ymin")[0].firstChild.data
-            xmax = object.getElementsByTagName("xmax")[0].firstChild.data
-            ymax = object.getElementsByTagName("ymax")[0].firstChild.data
-            bndbox = [xmin, ymin, xmax, ymax]
-            # print(bndbox)
-            bndboxs.append(bndbox)
-
         metal_imperfections = MetalImperfections()
-        bb_results = metal_imperfections.getBndbox(path_image, label)
+        imperfection,  bb_results = metal_imperfections.recognize(path_image)
 
-
-        # Se calculan las intersecciones
-        intersections = 0
-        for bb_result in bb_results:
-            for bndbox in bndboxs:
-                xA = max(bb_result[0], bndbox[0])
-                yA = max(bb_result[1], bndbox[1])
-                xB = min(bb_result[2], bndbox[2])
-                yB = min(bb_result[3], bndbox[3])
-
-                # compute the area of intersection rectangle
-                interArea = abs(max((xB - xA, 0)) * max((yB - yA), 0))
-                if interArea > 0:
-                    intersections += 1
+        bndboxs = __get_gt_bndbox__(path_anno)
+        intersections = __get_intersection__(bb_results, bndboxs)
 
         # Al menos, tiene que haber una intersección
-        self.assertTrue(intersections > 0)
-
-
-    def test_recognize(self):
-        # Cambiar el path y el label por otras imágenes (patches, scratches, ...)
-        path_image = './NEU-DET/IMAGES/inclusion_1.jpg'
-        label = 'inclusion'
-        metal_imperfections = MetalImperfections()
-        result = metal_imperfections.recognize(path_image)
-        self.assertEqual(result, label)
+        self.assertTrue(imperfection==label and intersections > 0)
 
     def test_classifier(self):
-        dir_test_path = './NEU-DET/IMAGES/test/inclusion/'
-        label = 'inclusion'
-        files = os.listdir(dir_test_path)
+        '''
+        Comprueba que la tasa de acierto de las etiquetas supere un umbral sobre unas imágenes de test
+        :return:
+        '''
+        dir_test_paths = ['./NEU-DET/IMAGES/test/other/',
+                          './NEU-DET/IMAGES/test/inclusion/',
+                          './NEU-DET/IMAGES/test/patches/',
+                          './NEU-DET/IMAGES/test/scratches/']
+        labels = ['other',
+                  'inclusion',
+                  'patches',
+                  'scratches']
 
-        metal_imperfections = MetalImperfections()
         success = 0
         failure = 0
-        for file in files:
-            result = metal_imperfections.recognize(file)
-            if result == label:
-                success += 1
-            else:
-                failure += 1
-        total = success + failure
-        if total == 0:
-            self.assertFalse(total == 0)
-        else:
-            self.assertTrue(1.0 * success / total > 0.95)
+        total = 0
+        for dir_test_path, label in zip(dir_test_paths, labels):
+            success_t, failure_t = __test_classifier__(dir_test_path, label)
+            success += success_t
+            failure += failure_t
+            total += success + failure
+
+        self.assertTrue(1.0 * success / total > 0.95)
 
 
 if __name__ == '__main__':
