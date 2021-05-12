1f642d01e410d5cb21337e093019d55c07218ae9
juanlu-urjc <jl.carrillo.2020@alumnos.urjc.es>
2021-05-12 17:14:41 +0200

    #16 Nuevo bug detectado

--- Application.py
+++ Application.py
@@ -17,16 +17,20 @@ if __name__ == '__main__':
 
     for image in images:
         # Recognize the image getting the imperfection type and the bounding boxes
-        im_gray=cv2.imread(args.images_path + image, cv2.IMREAD_GRAYSCALE)
-        label, bndboxs = metalImperfections.recognize(im_gray)
+        image_read = cv2.imread(args.images_path + image, cv2.IMREAD_COLOR)
+        label, bndboxs = metalImperfections.recognize(image_read)
+
         # Read the image to show it
-        image_read = cv.imread(args.images_path + image)
+        #image_read = cv2.imread(args.images_path + image)
         print("Imagen:", args.images_path + image, "Label y bndboxs", label, bndboxs)
         for x, y, xmax, ymax in bndboxs:
             # Print the bounding boxes in the image to show it
             cv2.rectangle(image_read,(x,y),(xmax,ymax),(200,0,0),2)
-        cv2.imshow(label, image_read)
+        cv2.imshow(label+'. Pulsar "q" para salir', image_read)
         # Wait for user key pressed 
-        cv2.waitKey(0)
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            break
         cv2.destroyAllWindows()
+        
+    cv2.destroyAllWindows()
         
